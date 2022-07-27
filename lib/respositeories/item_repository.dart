3648b6ed400508a18f73b59ce0c31df883098bce
Dariusz Korybski <korybskidariusz@gmail.com>
2022-07-27 18:59:49 +0200

    Add Details Page

--- lib/respositeories/item_repository.dart
+++ lib/respositeories/item_repository.dart
@@ -20,25 +20,33 @@ class ItemsRepository {
       ).toList();
     });
   }
+
   Future<void> delete({required String id}) {
-    return FirebaseFirestore.instance
-          .collection('items')
-          .doc(id)
-          .delete();
+    return FirebaseFirestore.instance.collection('items').doc(id).delete();
   }
+
+  Future<ItemModel> get({required String id}) async {
+    final doc =
+        await FirebaseFirestore.instance.collection('items').doc(id).get();
+    return ItemModel(
+      id: doc.id,
+      title: doc['title'],
+      adresURL: doc['image_url'],
+      relaseDate: (doc['relase_date'] as Timestamp).toDate(),
+    );
+  }
+
   Future<void> add(
     String title,
     String imageURL,
     DateTime releaseDate,
-  ) async{
-      await FirebaseFirestore.instance.collection('items').add(
-        {
-          'title': title,
-          'image_url': imageURL,
-          'release_date': releaseDate,
-        },
-      );
-     
+  ) async {
+    await FirebaseFirestore.instance.collection('items').add(
+      {
+        'title': title,
+        'image_url': imageURL,
+        'release_date': releaseDate,
+      },
+    );
   }
-
 }
